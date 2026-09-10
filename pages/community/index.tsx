7b429d0a9fea911a924dcc9c03bb3304b21bf5b1
sardor <sardor@sardors-MacBook-Pro.local>
2026-09-10 23:58:53 +0900

    feat: orginaze ans test pages router system

---
+++ pages/community/index.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import { useState } from "react";
+
+const Community = () => {
+    console.log("COMMUNITY COMPONENT - PAGES ROUTER")
+  const [title, setTitle] = useState<string>("hello");
+
+  return (
+    <div>
+      COMMUNITY{" "}
+      <button
+        onClick={() => alert("Hello MIT")}
+        style={{ margin: "15px" }}
+      >
+        Press
+      </button>
+    </div>
+  );
+};
+
+export default Community; 
---
+++ pages/property/index.tsx
@@ -0,0 +1,6 @@
+const PropertyList = () => {
+  console.log("PROPERTYLIST COMPONENT - PAGES ROUTER")
+  return <div> PROPERTY LIST</div>;
+};
+
+export default PropertyList
