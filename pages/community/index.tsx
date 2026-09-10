9d9b1d7251833996cbb5d2daf34853967ce63180
sardor <sardor@sardors-MacBook-Pro.local>
2026-09-11 01:54:51 +0900

    feat: integration Header and Footer via Layouts

--- pages/community/index.tsx
+++ pages/community/index.tsx
@@ -1,11 +1,10 @@
-"use client";
-
+import withLayoutBasic from "@/libs/components/layout/LayoutBasic";
+import { NextPage } from "next";
 import { useState } from "react";
 
-const Community = () => {
-    console.log("COMMUNITY COMPONENT - PAGES ROUTER")
+const Community: NextPage = () => {
+  // console.log("COMMUNITY COMPONENT - PAGES ROUTER")
   const [title, setTitle] = useState<string>("hello");
-
   return (
     <div>
       COMMUNITY{" "}
@@ -19,4 +18,4 @@ const Community = () => {
   );
 };
 
-export default Community; 
+export default withLayoutBasic(Community); 
--- pages/property/index.tsx
+++ pages/property/index.tsx
@@ -1,6 +1,10 @@
-const PropertyList = () => {
-  console.log("PROPERTYLIST COMPONENT - PAGES ROUTER")
-  return <div> PROPERTY LIST</div>;
+import withLayoutBasic from "@/libs/components/layout/LayoutBasic";
+import { Container, Stack } from "@mui/material";
+import { NextPage } from "next";
+
+const PropertyList: NextPage = () => {
+//   console.log("PROPERTYLIST COMPONENT - PAGES ROUTER");
+  return (<Container>PROPERTY LIST</Container>);
 };
 
-export default PropertyList
+export default withLayoutBasic(PropertyList);
